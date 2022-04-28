3ad1816d3a34ff04f3d6e7ec9392421c4df86cbd
JoseEscurra <103553484+JoseEscurra@users.noreply.github.com>
2022-04-28 16:26:11 -0500

    Add files via upload

---
+++ Bala_mediana.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala_mediana : MonoBehaviour
+{
+    public float velX;
+    private float velY = 0;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        Destroy(this.gameObject, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(velX, velY);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var tag = collision.gameObject.tag;
+        if(tag == "zombie")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+
+}
---
+++ zombie_vida.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zombie_vida : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int lifeZombie;
+
+
+    void Update()
+    {
+        CheckLife();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var tag = collision.gameObject.tag;
+        if(tag == "bala")
+        {
+            lifeZombie--;
+        }
+
+        if(tag == "balaMediana")
+        {
+            lifeZombie -= 2;
+        }
+
+        if(tag == "balaGrande")
+        {
+            lifeZombie = 0;
+        }
+    }
+
+    private void CheckLife()
+    {
+        if(lifeZombie <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
